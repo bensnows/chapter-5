0e57796d4619ce15e9a6af409a270f0e91b36fb6
陳新(Hsin Chen) <1909002@iisigroup.com>
2022-04-24 23:31:55 +0800

    增加minikube在 windows 設置

--- example-1/history/src/index.js
+++ example-1/history/src/index.js
@@ -14,7 +14,7 @@ function startHttpServer() {
     return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
         const app = express();
         setupHandlers(app);
-        
+
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
         app.listen(port, () => {
             resolve(); // HTTP server is listening, resolve the promise.
@@ -26,13 +26,13 @@ function startHttpServer() {
 // Application entry point.
 //
 function main() {
-    console.log("Hello world!");
-    
+    console.log("Hello world!!~~~~");
+
     return startHttpServer();
 }
 
 main()
-    .then(() => console.log("Microservice online."))
+    .then(() => console.log("Microservice online~"))
     .catch(err => {
         console.error("Microservice failed to start.");
         console.error(err && err.stack || err);
